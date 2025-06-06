99bf8486861bf40b572dace1aede2a42c603c962
Mauricio Cardoso <dev.mauriciocardoso@gmail.com>
2025-06-05 20:52:55 -0400

    feat(user): add user management

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,16 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Models\User;
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/user', [UserController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/check', [AuthController::class, 'check']);
 
-    Route::get('/user', function (Request $request): User {
-        return $request->user();
-    });
+    Route::apiResource('user', UserController::class)->except('index', 'store');
 });
